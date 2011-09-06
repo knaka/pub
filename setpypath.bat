0ab812a09b0a747c9732a5d8995177e235273460
Kiichiro NAKA <knaka@ayutaya.com>
2011-09-06 12:24:02 +0900

    -

--- setpypath.bat
+++ setpypath.bat
@@ -4,8 +4,8 @@ for %%P in (python.exe) do (
     for %%_ in (
       C:\python27 ^
       C:\python26 ^
-      C:\priv\prog\python31
-      C:\priv\prog\python27
+      C:\priv\prog\python31 ^
+      C:\priv\prog\python27 ^
       C:\priv\prog\python26
     ) do (
       if exist %%_ (
